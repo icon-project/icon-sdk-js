1536fcd5da75ea25b84043ed95f2a719fc9e389d
Sanghoo Park <sanghoo@icon.foundation>
2018-10-21 04:52:43 +0900

    Implement all kinds of transaction and re-structure files and directory

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 import IconService from './lib/IconService'
+import * as Amount from "./lib/data/Amount"
+import * as Converter from "./lib/data/Converter"
+
 export default IconService;
+export const IconAmount = Amount
+export const IconConverter = Converter
