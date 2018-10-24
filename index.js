ea16f6bf3c9903f839664f9f43437c7e23991730
Sanghoo Park <sanghoo@icon.foundation>
2018-10-24 11:44:28 +0900

    Modify Amount.js like java sdk

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import IconService from './lib/IconService'
-import * as Amount from "./lib/data/Amount"
+import Amount from './lib/data/Amount'
 import * as Converter from "./lib/data/Converter"
+import HttpProvider from './lib/transport/http/HttpProvider';
 
 export default IconService;
 export const IconAmount = Amount
 export const IconConverter = Converter
+export const IconHttpProvider = HttpProvider
