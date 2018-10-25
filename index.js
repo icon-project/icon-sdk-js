2e7f2d81ef58cbc68f7601147241be91e017e3e7
Jinhyun Kim <couldseeme@icon.foundation>
2018-10-26 07:39:16 +0900

    Fix aslant error

--- index.js
+++ index.js
@@ -15,12 +15,12 @@
  * limitations under the License.
  */
 
- import IconService from './lib/IconService'
-import Amount from './lib/data/Amount'
-import * as Converter from "./lib/data/Converter"
+import IconService from './lib/IconService';
+import Amount from './lib/data/Amount';
+import * as Converter from './lib/data/Converter';
 import HttpProvider from './lib/transport/http/HttpProvider';
 
 export default IconService;
-export const IconAmount = Amount
-export const IconConverter = Converter
-export const IconHttpProvider = HttpProvider
+export const IconAmount = Amount;
+export const IconConverter = Converter;
+export const IconHttpProvider = HttpProvider;
