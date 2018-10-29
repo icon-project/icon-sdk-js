e338190bcdb018e95060a21dd9cb5acba70664e6
Jinhyun Kim <couldseeme@icon.foundation>
2018-10-29 17:18:01 +0900

    Update directory of amount, converter, provider module

--- index.js
+++ index.js
@@ -16,11 +16,5 @@
  */
 
 import IconService from './lib/IconService';
-import Amount from './lib/data/Amount';
-import * as Converter from './lib/data/Converter';
-import HttpProvider from './lib/transport/http/HttpProvider';
 
 export default IconService;
-export const IconAmount = Amount;
-export const IconConverter = Converter;
-export const IconHttpProvider = HttpProvider;
