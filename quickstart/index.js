29dfe740e5d8e4fdd998aa505b0767eed2b95970
Jinhyun Kim <couldseeme@icon.foundation>
2018-10-30 18:44:12 +0900

    Delete unused import code

--- quickstart/index.js
+++ quickstart/index.js
@@ -1,6 +1,5 @@
 /* eslint-disable */
 import WalletExample from './example/js/WalletExample.js';
 import IcxTransactionExample from './example/js/IcxTransactionExample.js';
-import TokenTrasnactionExample from './example/js/TokenTransactionExample.js';
 import DeployAndTransferTokenExample from './example/js/DeployAndTransferTokenExample.js';
 import SyncBlockExample from './example/js/SyncBlockExample.js';
