710889c32844bf3ad92a8eca723666a9ffa4e1eb
Sanghoo Park <sanghoo@icon.foundation>
2019-02-11 20:25:30 +0900

    Modify test code for getTransactionResult

--- test/IconService.getTransactionResult.js
+++ test/IconService.getTransactionResult.js
@@ -2,13 +2,17 @@ import assert from 'assert';
 import IconService, { HttpProvider, IconValidator } from '../build/icon-sdk-js.node.min';
 const iconService = new IconService(new HttpProvider('https://bicon.net.solidwallet.io/api/v3'));
 
-const TestTransactionHash = '0x260d117d3b695bd10b132b43ac54341b3c19bdcc6671ebaa8beaf982a79051c8';
+const tests = [
+	'0x260d117d3b695bd10b132b43ac54341b3c19bdcc6671ebaa8beaf982a79051c8',
+]
 
 describe('IconService', () => {
 	describe('getTransactionResult()', () => {
-		it('should return the right transaction', async () => {
-			const transaction = await iconService.getTransactionResult(TestTransactionHash).execute();
-			assert.ok(IconValidator.isTransactionResult(transaction));
+		tests.forEach((test) => {
+			it('should return the right transaction', async () => {
+				const transaction = await iconService.getTransactionResult(test).execute();
+				assert.ok(IconValidator.isTransactionResult(transaction));
+			});
 		});
 	});
 });
