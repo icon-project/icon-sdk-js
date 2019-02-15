02ebf17f0ad9d6fa81eee6411f3618704fdbb3dd
Sanghoo Park <sanghoo@icon.foundation>
2019-02-15 16:48:25 +0900

    Support for searching v2 block

--- test/IconService.getBlockByHeight.js
+++ test/IconService.getBlockByHeight.js
@@ -1,17 +1,27 @@
 import assert from 'assert';
 import IconService, { HttpProvider, IconConverter } from '../build/icon-sdk-js.node.min';
-
-const iconService = new IconService(new HttpProvider('https://bicon.net.solidwallet.io/api/v3'));
-
-const tests = [777, IconConverter.toBigNumber("777")]
+const providers = [
+    'https://bicon.net.solidwallet.io/api/v3',
+    'https://test-ctz.solidwallet.io/api/v3',
+    'https://ctz.solidwallet.io/api/v3'
+]
+const tests = [
+    10,
+    777,
+    IconConverter.toBigNumber("777")
+]
 
 describe('IconService', () => {
     describe('getBlockByHeight()', () => {
-        tests.forEach((test) => {
-            it('should return the right block', async () => {
-                const block = await iconService.getBlockByHeight(test).execute();
-                assert.ok(!!block);
+        providers.forEach(provider => {
+            const iconService = new IconService(new HttpProvider(provider));
+            tests.forEach(test => {
+                it('should return the right block', async () => {
+                    const block = await iconService.getBlockByHeight(test).execute();
+                    console.log(block)
+                    assert.ok(!!block);
+                });
             });
-        });
+        })
     });
 });
