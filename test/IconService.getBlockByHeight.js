61fee89857a2f3eb61827fc58dfee80057493106
JH Kim <couldseeme@icon.foundation>
2019-11-15 15:21:48 +0900

    Fix getBlockByHeight bug returning undefined txHash

--- test/IconService.getBlockByHeight.js
+++ test/IconService.getBlockByHeight.js
@@ -1,26 +1,47 @@
 import assert from 'assert';
 import IconService, { HttpProvider, IconConverter } from '../build/icon-sdk-js.node.min';
+
 const providers = [
-    'https://bicon.net.solidwallet.io/api/v3',
-    'https://test-ctz.solidwallet.io/api/v3',
-    'https://ctz.solidwallet.io/api/v3'
-]
+	'https://bicon.net.solidwallet.io/api/v3',
+	'https://test-ctz.solidwallet.io/api/v3',
+	'https://ctz.solidwallet.io/api/v3',
+];
+
 const tests = [
-    10,
-    777,
-    IconConverter.toBigNumber("777")
-]
+	10,
+	777,
+	IconConverter.toBigNumber('777'),
+];
 
 describe('IconService', () => {
-    describe('getBlockByHeight()', () => {
-        providers.forEach(provider => {
-            const iconService = new IconService(new HttpProvider(provider));
-            tests.forEach(test => {
-                it('should return the right block', async () => {
-                    const block = await iconService.getBlockByHeight(test).execute();
-                    assert.ok(!!block);
-                });
-            });
-        })
-    });
+	describe('getBlockByHeight()', () => {
+		providers.forEach((provider) => {
+			const iconService = new IconService(new HttpProvider(provider));
+			tests.forEach((test) => {
+				it('should return the right block', async () => {
+					const block = await iconService.getBlockByHeight(test).execute();
+					assert.ok(!!block);
+				});
+			});
+		});
+	});
+});
+
+const mainnetTests = [
+	...tests,
+	9217169,
+	10000000,
+];
+
+describe('IconService', () => {
+	describe('getBlockByHeight() on Mainnet', () => {
+		const iconService = new IconService(new HttpProvider(providers[2]));
+		mainnetTests.forEach((test) => {
+			it('should return the right block', async () => {
+				const block = await iconService.getBlockByHeight(test).execute();
+				console.log(JSON.stringify(block, null, 2));
+				assert.ok(!!block);
+			});
+		});
+	});
 });
